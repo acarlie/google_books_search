3ebb210e6c50731a155dda9fe87e9cd94731bfdb
acarlie <ameliacarlie@gmail.com>
2019-11-09 11:15:27 -0500

    Initial Files

---
+++ client/src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+//Pages
+import Home from './components/pages/Home';
+import Saved from './components/pages/Saved';
+import Book from './components/pages/Book';
+import Nav from './components/Nav';
+import NoMatch from './components/pages/NoMatch';
+
+function App() {
+  return (
+    <div>
+      <Router>
+        <div>
+          <Nav />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/saved" component={Saved} />
+            <Route exact path="/books/:id" component={Book} />
+            <Route component={NoMatch} />
+          </Switch>
+        </div>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
